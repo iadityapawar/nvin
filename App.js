e9dd2960a50c6884102c23221c3a9c7c30bc5b80
GitPass@4321 <iaditya4912pawar@gmail.com>
2023-05-11 19:25:13 +0530

    commitNeww

--- App.js
+++ App.js
@@ -5,52 +5,58 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  TouchableOpacity,
 } from 'react-native';
 
-import React, {useState, useEffect} from 'react';
+import React, {
+  useState,
+  useEffect
+} from 'react';
 //import {useFonts} from 'expo-font';
 import AppLoading from "expo-app-loading";
 
 export default function App() {
-  
-  const [count, setCount] = useState(1);
-  
+
+  const [count,
+    setCount] = useState(1);
+
   // useEffect(()=>{
   //   async function loadFonts(){
   //     await Font.loadAsync({
   //       'Inter-Bold': require("./assets/fonts/Inter-Bold.ttf"),
   //       ),
-        
+
   //     }).then(res=>{
   //       console.log("FONTS LOADED!");
   //       setLoaded(true)
   //     }).catch(Err=>{
   //       setLoaded(true);
   //       console.log(Err);
-  //     }); 
+  //     });
   //   }
 
   //   loadFonts();
   // },[])
-  
+
   // const Heading = (color) => {
   //   return({fontSize:45*fontScale, fontFamily: "Inter-Bold", color, marginTop: -10, marginLeft: InitialMargin, letterSpacing: 4})
   // }
-  
 
-//let [fontsLoaded] = useFonts({
-//      'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
-//    });
-//    if (!fontsLoaded) {
-//        return <AppLoading/>;
-//    }
-  
+
+  //let [fontsLoaded] = useFonts({
+  //      'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+  //    });
+  //    if (!fontsLoaded) {
+  //        return <AppLoading/>;
+  //    }
+
   return (
     <View style={styles.container}>
+      // <View style={flexDirection: 'horizontal', alignItems: 'center', backgroundColor: 'aqua', justifyContent: 'center'}><View style={width: '40%', justifyContent: 'center'}><Text>Hello</Text></View><View style={width: '40%', justifyContent: 'center'}><Text>Hello</Text></View></View>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text style={{fontSize:19}}>'You are the Mine' + {count}</Text>
-      <Button title='CLICK' onPress={()=>setCount(count+1)}/>
+      <Text style={ { fontSize: 19 }}>'You are the Mine' + {count}</Text>
+      <Button title='CLICK' onPress={()=>setCount(count+1)} />
       <StatusBar style="auto" />
     </View>
   );
